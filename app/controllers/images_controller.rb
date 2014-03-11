9da282a888c9186c463a7a48fdd1993f3634ff21
Alex Fox <alexdysonfox@gmail.com>
2014-03-11 14:25:34 -0400

    create galleries, upload images

---
+++ app/controllers/images_controller.rb
@@ -0,0 +1,23 @@
+class ImagesController < ApplicationController
+
+  def new
+    @gallery = Gallery.find(params[:gallery_id])
+    @image = Image.new
+  end
+
+  def create
+    gallery = Gallery.find(params[:gallery_id])
+    gallery.images.create(image_params)
+
+    redirect_to gallery 
+  end
+
+  def show 
+  end
+
+  private
+
+  def image_params
+    params.require(:image).permit(:name, :description, :url)
+  end
+end
