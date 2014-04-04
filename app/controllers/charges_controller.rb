378904cb82d8b6fb867d4718fd301ab72ba3a72b
Alex Fox <alexdysonfox@gmail.com>
2014-04-04 13:38:35 -0400

    Refactored Charges controller

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,33 +1,7 @@
 class ChargesController < ApplicationController
-def new
- 
-end
-
-def create
-  # Amount in cents
-  @amount = 500
-
-  customer = Stripe::Customer.create(
-    :email => current_user.email,
-    :card  => params[:stripeToken]
-  )
-
-  charge = Stripe::Charge.create(
-    :customer    => customer.id,
-    :amount      => @amount,
-    :description => 'Rails Stripe customer',
-    :currency    => 'usd'
-  )
-
-  rescue Stripe::CardError => e
-    flash[:error] = e.message
-    redirect_to charges_path 
-  else 
-
-  current_user.stripe_id = customer.id
-  current_user.save
-  
-  flash[:notice] = "THANKS FOR YOUR MONEY!!!!!!"
-  redirect_to dashboard_path
+  def create
+    charge = Charge.new(current_user, params[:stripeToken])
+    charge.process
+    redirect_to dashboard_path
   end
 end
---
+++ app/models/charge.rb
@@ -0,0 +1,35 @@
+class Charge
+
+  AMOUNT = 500
+
+  def initialize(user, token)
+    @user = user
+    @token = token
+  end
+
+  def process
+    customer = create_customer
+    create_charge customer
+    user.update(stripe_id: customer.id)
+  end
+
+  private
+
+  attr_reader :user, :token
+
+  def create_customer
+    Stripe::Customer.create(
+      :email => user.email,
+      :card  => token
+    )
+  end
+
+  def create_charge(customer)
+    Stripe::Charge.create(
+      :customer    => customer.id,
+      :amount      => Charge::AMOUNT,
+      :description => 'Become a premium member',
+      :currency    => 'usd'
+    )
+  end
+end
